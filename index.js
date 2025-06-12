e3d26e5dbf88038efabfef77adbe4d102012f050
Your Name <you@example.com>
2025-06-12 18:39:00 +0300

    added my API

--- index.js
+++ index.js
@@ -1,16 +1,70 @@
 import express from 'express';
-import {prismaclient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const app = express()
+const app = express();
+const prisma = new PrismaClient();
 
-let port;
-if(process.env.port){
-    port = process.env.PORT
 
-}else{
-    port
-}
-app.listen(4000,() => {
-    console.log ('App running on port 4000');
-}
+app.use(express.json());
+// post ama create
+app.post('/tasks', async (req, res) => {
+  const { title, description } = req.body;
+  try {
+    const task = await prisma.task.create({
+      data: { title, description },
+    });
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ error: 'failed to create taskss' });
+  }
+});
+// Get all
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await prisma.task.findMany();
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch tasks' });
+  }
+});
 
+// Get 
+app.get('/tasks/:id', async (req, res) => {
+  const { id } = req.params;
+  const task = await prisma.task.findUnique({ where: { id } });
+  if (!task) {
+    return res.status(404).json({ error: 'Task not found' });
+  }
+  res.json(task);
+});
+
+// Update 
+app.put('/tasks/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, description, isCompleted } = req.body;
+
+  try {
+    const updatedTask = await prisma.task.update({
+      where: { id: Number(id) },
+      data: { title, description, isCompleted },
+    });
+    res.json(updatedTask);
+  } catch (error) {
+    res.status(500).json({ error: 'Task not found or update failed' });
+  }
+});
+// Delete
+app.delete('/tasks/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    await prisma.task.delete({ where: { id } });
+    res.json({ message: 'Task deleted successfully' });
+  } catch (error) {
+    res.status(404).json({ error: 'Task not found' });
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at ${PORT}`);
+});
